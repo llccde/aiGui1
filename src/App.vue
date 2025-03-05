3df35899c9e0a1eb777396e6780b62f1f8aed846
llbb <aaa15305404873@gmail.com>
2025-03-05 22:58:53 +0800

    11111

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script lang="ts" setup>
 
-import JSONLoader from './components/JSONLoader.vue';
 import { provide, ref } from 'vue';
-import Manager from './components/Manager.vue';
-import FlagOutput from  "./ts/FlagOutPut";
 import ApiTest from './components/ApiTest.vue';
+import defaultLayout from './components/layoutComponent/defaultLayout.vue';
+import smart_container from './components/layoutComponent/smart_container.vue';
+import SDiv from './components/layoutComponent/SDiv.vue';
 provide('isDebug', ref(true));
 </script>
 
@@ -16,10 +16,14 @@ provide('isDebug', ref(true));
   <main>
     
     <div id="app" style="height: 100vh; width: 100vw;">
-      <Manager >  
-        <JSONLoader />
-      </Manager>
-      <!-- <ApiTest /> -->
+      <SDiv style-class = "head_side_body_foot">
+        <p position="header">bottom</p>
+        <p position="header">head</p>
+        <p position="sidebar">side</p>
+        <p position="main">main</p>
+        <p position="footer">footer</p>
+      </SDiv>
+
       
     </div>
   </main>
--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import { createApp, inject, provide } from 'vue'
 import App from './App.vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUser, faHome } from '@fortawesome/free-solid-svg-icons'
-import BoolButton from './components/states_test/BoolButton.vue'
-import IntSlider from './components/states_test/IntSlider.vue'
-import EnumList from './components/states_test/EnumList.vue'
-
+import smart_container from './components/layoutComponent/smart_container.vue'
+import head_side_body_foot from "./components/layoutComponent/head_side_body_foot.vue"
 const app = createApp(App)
 //createApp(App).mount('#app')
 library.add(faUser, faHome)
 
-app.component('BoolButton', BoolButton);
-app.component('IntSlider', IntSlider);
-app.component('EnumList', EnumList);
+// app.component('BoolButton', BoolButton);
+// app.component('IntSlider', IntSlider);
+// app.component('EnumList', EnumList);
+app.component("smart_container",smart_container);
+app.component("head_side_body_foot",head_side_body_foot);
 app.mount('#app')
